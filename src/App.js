50c06e62bbfdb30848e75d8ab84d945120b23bb6
TinchoLobos <martinlobos0505@gmail.com>
2022-04-25 21:56:48 -0300

    uploading Check List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name,setName] = useState("");
+  const [list,setList] = useState([]);
+  const [isEditing,setIsEditing] = useState(false);
+  const [editID,setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show:false,
+    msg:'',
+    type:''});
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+  if(!name){
+   showAlert(true,"danger","please enter value")
+  }
+
+  else if(name && isEditing){
+  setList(list.map((item) =>{
+  if(item.id === editID){
+    return {...item,title:name}
+  }
+  return item
+  }))
+  setName('');
+  setEditID(null);
+  setIsEditing(false);
+  showAlert(true,"success","item changed")
+  }
+
+else{
+  showAlert(true,"success","item added")
+  const newItem = {id: new Date().getTime().toString(),title:name}
+  setList([...list,newItem]);
+  setName('');
+}
+  }
+
+const showAlert = (show=false,type="",msg="") =>{
+  setAlert({show,type,msg})
 }
 
-export default App;
+const clearList = () =>{
+  showAlert(true,'danger','empty list')
+  setList([])
+}
+
+const removeItem = (id) =>{
+showAlert(true,'danger','object removed')
+const newList = list.filter((item) =>{
+  return item.id !== id
+})
+setList(newList)
+}
+
+const editItem = (id) =>{
+  const specificItem = list.find((item) => item.id === id);
+  setIsEditing(true)
+  setEditID(id)
+  setName(specificItem.title)
+}
+  return <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert 
+      {...alert} 
+      removeAlert={showAlert}
+      list={list}
+      />}
+      <h3>My check List</h3>
+      <div className="form-control">
+        <input 
+        type="text"
+        className='grocery'
+        placeholder='e.g pay gym membership'
+        value= {name}
+        onChange={(e)=> setName(e.target.value)}
+        
+        />
+        <button type='submit' className='submit-btn'>
+          {isEditing ? 'edit': 'submit'}
+        </button>
+      </div>
+    </form>
+    <div className='grocery-container'>
+      <List
+      items={list}
+      removeItem={removeItem}
+      editItem={editItem}
+      />
+      <button className='clear-btn' onClick={clearList}> Clear Items</button>
+    </div>
+  </section>
+}
+
+export default App
